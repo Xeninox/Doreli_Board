6b9485972f352f1b86402f680c39ced013f4b57d
chrisasante <chrisas52@gmail.com>
2017-04-19 22:51:48 +0000

    user layout committed

---
+++ layout/header.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+  <head>
+  <title></title>
+  </head>
+  	<body>
+	    <!-- Fixed navbar -->
+	    <div class="navbar navbar-default navbar-fixed-top" role="navigation">
+	      <div class="container">
+	        <div class="navbar-header">
+	          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+	            <span class="sr-only">Toggle navigation</span>
+	            <span class="icon-bar"></span>
+	            <span class="icon-bar"></span>
+	            <span class="icon-bar"></span>
+	          </button>
+	          <a class="navbar-brand" href="index.html">Doreli Board</a>
+	        </div>
+	        <div class="navbar-collapse collapse navbar-right">
+	          <ul class="nav navbar-nav">
+	            <li><a href="user_home.html">HOME</a></li>
+	            <li><a href="upload.html">UPLOAD</a></li>
+	            <li class="dropdown active">
+	              <a href="#" class="dropdown-toggle" data-toggle="dropdown"><img src="../image/user.jpg" id="pic"><b class="caret"></b></a>
+
+	              <ul class="dropdown-menu">
+	                <li><a href="my_post.html">MY POSTS</a></li>
+	                <li><a href="#">LOGOUT</a></li>
+	              </ul>
+	            </li>
+	          </ul>
+	        </div><!--/.nav-collapse -->
+	      </div>
+	    </div>
+  </body>
+</html>
